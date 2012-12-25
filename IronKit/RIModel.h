398ffec89fd4364e62e0571cea44922e429ee257
Ali Gadzhiev <6acypMaH@gmail.com>
2012-12-25 18:04:56 +0400

    Added Model abstraction

---
+++ IronKit/RIModel.h
@@ -0,0 +1,28 @@
+//
+//  RIModel.h
+//  DropboxSample
+//
+//  Created by Ali Gadzhiev on 12/25/12.
+//  Copyright (c) 2012 Red Iron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const NIModelDidFinishLoadNotification;
+extern NSString * const NIModelDidFailLoadNotification;
+
+@interface RIModel : NSObject {
+	__block BOOL _loading;
+	__block BOOL _loaded;
+}
+
+@property (nonatomic, readonly, getter = isLoading) BOOL loading;
+@property (nonatomic, readonly, getter = isLoaded) BOOL loaded;
+
+- (void)load;
+- (void)cancelLoad;
+
+- (void)didFinishLoadWithResponse:(id)response;
+- (void)didFailLoadWithError:(NSError *)error canceled:(BOOL)canceled;
+
+@end
---
+++ IronKit/RIModel.m
@@ -0,0 +1,69 @@
+//
+//  RIModel.m
+//  DropboxSample
+//
+//  Created by Ali Gadzhiev on 12/25/12.
+//  Copyright (c) 2012 Red Iron. All rights reserved.
+//
+
+#import "RIModel.h"
+
+NSString * const NIModelDidFinishLoadNotification	= @"com.ironkit.ModelDidFinishLoad";
+NSString * const NIModelDidFailLoadNotification		= @"com.ironkit.ModelDidFailLoad";
+
+@implementation RIModel
+
+- (void)load {
+	
+}
+
+- (void)cancelLoad {
+	[self didFailLoadWithError:nil canceled:YES];
+}
+
+#pragma mark - Loading end points
+
+- (void)didFinishLoadWithResponse:(id)response {
+	void (^block)(void) = ^{
+		_loading = NO;
+		_loaded = YES;
+		
+		NSDictionary * userInfo = nil;
+		if (response) {
+			userInfo = @{ @"response" : response };
+		}
+		[[NSNotificationCenter defaultCenter] postNotificationName:NIModelDidFinishLoadNotification
+															object:self
+														  userInfo:userInfo];
+	};
+	
+	if ([NSThread isMainThread]) {
+		block();
+	} else {
+		dispatch_async(dispatch_get_main_queue(), block);
+	}
+}
+
+- (void)didFailLoadWithError:(NSError *)error canceled:(BOOL)canceled {
+	void (^block)(void) = ^{
+		_loading = NO;
+		_loaded |= NO;
+		
+		NSMutableDictionary * userInfo = [NSMutableDictionary dictionaryWithObject:[NSNumber numberWithBool:canceled]
+																			forKey:@"canceled"];
+		if (error) {
+			[userInfo setObject:error forKey:@"error"];
+		}
+		[[NSNotificationCenter defaultCenter] postNotificationName:NIModelDidFinishLoadNotification
+															object:self
+														  userInfo:userInfo];
+	};
+	
+	if ([NSThread isMainThread]) {
+		block();
+	} else {
+		dispatch_async(dispatch_get_main_queue(), block);
+	}
+}
+
+@end
